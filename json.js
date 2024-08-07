d4f67e49172609de2aa528af909cb5c06e011c7f
thefrox <thefrox@gmail.com>
2024-08-07 16:51:07 +0200

    Add files via upload

---
+++ json.js
@@ -0,0 +1,465 @@
+{
+    "props": {
+    "pageProps": {
+        "exported": false,
+            "site": "mcfnet",
+            "lang": "fr",
+            "path": "http://goole.fr/ecole-de-ski",
+            "menuPath": {
+            "active": true,
+                "disabled": false,
+                "name": "nos-ecoles",
+                "title": "Nos écoles",
+                "shortTitle": "Ecoles",
+                "menu": "http://goole.fr//ecole-de-ski",
+                "path": "http://goole.fr//ecole-de-ski",
+                "page": "http://goole.fr//master-ecole",
+                "prev": {
+                "active": true,
+                    "disabled": false,
+                    "name": "home",
+                    "title": "Accueil",
+                    "menu": "/",
+                    "path": "/",
+                    "page": "/"
+            }
+        },
+        "breadcrumb": {
+            "active": true,
+                "disabled": false,
+                "name": "home",
+                "title": "Accueil",
+                "menu": "/",
+                "path": "/",
+                "page": "/",
+                "next": {
+                "active": true,
+                    "disabled": false,
+                    "name": "nos-ecoles",
+                    "title": "Nos écoles",
+                    "shortTitle": "Ecoles",
+                    "menu": "http://goole.fr//ecole-de-ski",
+                    "path": "http://goole.fr//ecole-de-ski",
+                    "page": "/master-ecole"
+            }
+        },
+        "zones" : {},
+        "seo": {
+            "title": "",
+                "description": "",
+                "robots": "",
+                "canonical": "",
+                "keywords": ""
+        },
+        "metadata": "{}",
+            "listEcoles": [
+            {
+                "code": 264,
+                "name": "SARL ALPES AVENTURES",
+                "slug": "SARL ALPES AVENTURES",
+                "position": "45.857337,6.611618",
+                "adressel1": "1935 Route nationale 74120 Megève France",
+                "cp": "74120",
+                "city": "Megève",
+                "tel": "06 20 62 23 00",
+                "email": "direction@alpesaventures.com",
+                "moniteurs": {
+                    "total": null,
+                    "fr": null,
+                    "en": null,
+                    "es": null
+                },
+                "services": [
+                    {
+                        "id": 1,
+                        "name": "BMX"
+                    },
+                    {
+                        "id": 3,
+                        "name": "Gravel"
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": true,
+                    "url": null,
+                    "bookingUrl": null
+                },
+                "station": [
+                    {
+                        "name": "Cours particuliers",
+                        "id": 1
+                    },
+                    {
+                        "name": "Randonnée\/Balade",
+                        "id": 4
+                    }
+                ]
+            },
+            {
+                "code": 101,
+                "name": "Test Guil eBike VTT",
+                "slug": "AC'H Cyril",
+                "position": "46.280721856441566,6.7198572158814045",
+                "adressel1": "15, place d'Abondance",
+                "cp": "74360",
+                "city": "ABONDANCE",
+                "tel": "04 50 73 00 67",
+                "email": "info@mcf-abondance.com",
+                "moniteurs": {
+                    "total": 16,
+                    "fr": 17,
+                    "en": 12,
+                    "de": 1
+                },
+                "services": [
+                    {
+                        "id": 1,
+                        "name": "BMX"
+                    },
+                    {
+                        "id": 5,
+                        "name": "Route"
+                    },
+                    {
+                        "id": 6,
+                        "name": "VTT"
+                    },
+                    {
+                        "id": 8,
+                        "name": "VTT Enduro"
+                    },
+                    {
+                        "id": 9,
+                        "name": "VTT Électrique"
+                    }
+                ],
+                "station": [
+                    {
+                        "name": "Format Club",
+                        "id": 2
+                    },
+                    {
+                        "name": "Cours particuliers",
+                        "id": 1
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": true,
+                    "url": "https://www.mcf-abondance.com/",
+                    "bookingUrl": "https://www.mcf-abondance.com/"
+                }
+            },
+            {
+                "code": 104,
+                "name": "Abries (haut Guil)",
+                "slug": "haut-guil",
+                "position": "44.79410338678622,6.928147315978919",
+                "adressel1": "LE CHALET mcf",
+                "adressel2": "11 AVENUE DU VISO",
+                "cp": "05460",
+                "city": "ABRIES-RISTOLAS",
+                "tel": "04 92 43 56 31",
+                "email": "contact@mcf-abries.fr",
+                "moniteurs": {
+                    "total": 15,
+                    "fr": 14,
+                    "en": 4,
+                    "it": 4
+                },
+                "services": [
+                    {
+                        "id": 9,
+                        "name": "VTT Électrique"
+                    },
+                    {
+                        "id": 1,
+                        "name": "BMX"
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": true,
+                    "url": "https://www.mcf-abries.fr/",
+                    "bookingUrl": "https://www.mcf-abries.fr/"
+                },
+                "station": [
+                    {
+                        "name": "Stage",
+                        "id": 5
+                    }
+                ]
+            },
+            {
+                "code": 107,
+                "name": "Les Agudes",
+                "slug": "agudes",
+                "position": "42.7877646646778,0.4773532450198559",
+                "adressel1": "LE CHALET",
+                "cp": "31110",
+                "city": "GOUAUX DE LARBOUST",
+                "tel": "05 61 79 07 12",
+                "email": "mcf-les-agudes@wanadoo.fr",
+                "moniteurs": {
+                    "total": 33,
+                    "fr": 35,
+                    "en": 8,
+                    "es": 15
+                },
+                "services": [
+                    {
+                        "id": 3,
+                        "name": "Gravel"
+                    },
+                    {
+                        "name": "VTT Descente",
+                        "id": 7
+                    }
+
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": true,
+                    "url": "https://www.mcf-lesagudes.fr/fr/",
+                    "bookingUrl": "https://www.mcf-lesagudes.fr/fr/"
+                },
+                "station": [
+                    {
+                        "name": "Stage",
+                        "id": 5
+                    }
+                ]
+            },
+            {
+                "code": 113,
+                "name": "Les Aillons",
+                "slug": "les-aillons",
+                "position": "45.64290897393859,6.064089677124059",
+                "adressel1": "LA CORRERIE",
+                "cp": "73340",
+                "city": "AILLON LE JEUNE",
+                "tel": "04 79 54 62 50",
+                "email": "mcf.les.aillons.mail@gmail.com",
+                "moniteurs": {
+                    "total": 51,
+                    "fr": 43,
+                    "en": 36,
+                    "es": 1
+                },
+                "services": [
+                    {
+                        "id": 4,
+                        "name": "Mobilité/Remise en selle"
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": false,
+                    "url": "https://www.lesaillons.com/mcf.html",
+                    "bookingUrl": "https://widget.vente-en-ligne-mcf.com/les-aillons"
+                },
+                "station": [
+                    {
+                        "name": "Séminaire",
+                        "id": 6
+                    }
+                ]
+            },
+            {
+                "code": 116,
+                "name": "Aime La Plagne",
+                "slug": "aime-la-plagne",
+                "position": "45.51111803526909,6.66592695504761",
+                "adressel1": "Immeuble Plagne Aime 2000",
+                "cp": "73210",
+                "city": "Aime La Plagne",
+                "tel": "04 79 09 04 75",
+                "email": "info@mcfplagneaime2000.com",
+                "moniteurs": {
+                    "total": 120,
+                    "fr": 125,
+                    "en": 105,
+                    "it": 6,
+                    "es": 10,
+                    "nl": 2,
+                    "de": 4,
+                    "ru": 2,
+                    "pt": 1
+                },
+                "services": [
+                    {
+                        "id": 6
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": true,
+                    "url": "https://www.mcfplagneaime2000.com/",
+                    "bookingUrl": "https://www.mcfplagneaime2000.com/"
+                },
+                "station": [
+                    {
+                        "name": "Format Club",
+                        "id": 2
+                    }
+                ]
+            },
+            {
+                "code": 248,
+                "name": "Champ Du Feu",
+                "slug": "champ-du-feu",
+                "position": "48.39828529191092,7.2669300105590935",
+                "adressel1": "ROUTE DE LA SERVA",
+                "cp": "67130",
+                "city": "BELMONT",
+                "tel": "03 88 97 35 05",
+                "email": "mcf.champdufeu@gmail.com",
+                "moniteurs": {
+                    "total": 8
+                },
+                "services": [
+                    {
+                        "id": 2,
+                        "name": "Club Piou-Piou"
+                    },
+                    {
+                        "id": 8,
+                        "name": "Raquettes"
+                    },
+                    {
+                        "id": 10,
+                        "name": "Nordique"
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": false,
+                    "url": "https://www.mcf.net/ecole-de-ski/champ-du-feu",
+                    "bookingUrl": "https://widget.vente-en-ligne-mcf.com/champ-du-feu"
+                },
+                "station": [
+                    {
+                        "name": "Randonnée/Balade",
+                        "id": 4
+                    },
+                    {
+                        "name": "Formation",
+                        "id": 3
+                    }
+                ]
+            },
+            {
+                "code": 702,
+                "name": "Villard Reculas",
+                "slug": "villard-reculas",
+                "position": "43.617048921743745, 3.9122238105558615",
+                "adressel1": "65 COUR CELSIUS",
+                "cp": "34000",
+                "city": "MONTPELLIER",
+                "tel": "00 33 (0)476804001",
+                "email": "info@mcf-villard-reculas.com",
+                "moniteurs": {
+                    "total": 25,
+                    "fr": 23,
+                    "en": 19
+                },
+                "services": [
+                    {
+                        "id": 7,
+                        "name": "VTT Descente"
+                    }
+                ],
+                "website": {
+                    "fallback": false,
+                    "isMerchant": true,
+                    "url": "https://www.mcf-villard-reculas.com/fr/",
+                    "bookingUrl": "https://www.mcf-villard-reculas.com/fr/"
+                },
+                "station": [
+                    {
+                        "name": "Formation",
+                        "id": 3
+                    }
+                ]
+            },
+            {
+                "code": 703,
+                "name": "test societe montpell",
+                "slug": "test website montpell",
+                "position": "43.61891794447907, 3.9169836105559446",
+                "adressel1": "836 RUE DU MAS DE VERCHANT 34000 MONTPELLIER ",
+                "cp": "34000",
+                "city": "MONTPELLIER",
+                "tel": "00 33 (0)476804001",
+                "email": "info@mcf-villard-reculas.com",
+                "moniteurs": {
+                    "total": 25
+                },
+                "services": [
+                    {
+                        "name": "VTT Enduro",
+                        "id": 8
+                    }
+                ],
+                "website": {
+                    "url": "https://www.mcf-villard-reculas.com/fr/"
+                }
+            }
+        ],
+            "latLng": [
+            46.71109,
+            1.7191036
+        ],
+            "radius": "50",
+            "address": "",
+            "svcFilter": [],
+            "regionFilter": [],
+            "geolocateOnInit": "true"
+    }
+},
+    "page": "/master-ecole",
+    "query": {
+    "geolocateOnInit": "true",
+        "radius": "50"
+},
+    "buildId": "kvCiL5ohyNSagBC8AYH1G",
+    "runtimeConfig": {
+    "langs": [
+        "fr",
+        "en",
+        "es",
+        "nl",
+        "ru"
+    ],
+        "siteName": "mcf net",
+        "site": "mcfnet",
+        "port": 9231,
+        "ubloApi": "https://ubloapi.stargraf.com",
+        "langPrefix": {
+        "ubloRoutes": false,
+            "links": false
+    },
+    "disableClientRouting": true,
+        "domains": {
+        "fr": "https://www.mcf.net",
+            "en": "https://www.mcf-en.net",
+            "es": "https://mcf-es.es",
+            "nl": "https://www.mcf-nl.nl",
+            "ru": "https://www.mcf-ru.net"
+    },
+    "preProd": false,
+        "plausibleDomain": {
+        "fr": "mcf.net",
+            "en": "mcf-en.net",
+            "es": "mcf-es.es",
+            "nl": "mcf-nl.nl",
+            "ru": "mcf-ru.net"
+    },
+    "gaAccount": "UA-3174134-66",
+        "ubloMode": "server"
+},
+    "isFallback": false,
+    "customServer": true,
+    "gip": true
+}
